eef39f27e3c9205c29e5fecb4221475e50209a33
vic fir <vicfir@hotmail.fr>
2023-01-31 10:30:20 +0100

    menu change info with btn -> end section

--- public/script/main.js
+++ public/script/main.js
@@ -46,28 +46,6 @@ let navActive = document.getElementsByClassName("navActive");
 titleMenuL.innerText=`${navActive[0].innerText}`;
 titleMenu.appendChild(titleMenuL);
 
-for (let i = 0; i < navSelect.children.length; i++) {
-    navSelect.children.item(i).addEventListener("click", (e)=>{
-        
-        //delete old  menu active
-        navActive[0].className="";
-
-        //select menu active
-        e.target.className="navActive";
-
-        //title menu change
-        let titleMenuLSelect = document.querySelector(".titleMenu > h3");
-        titleMenuLSelect.innerText=`${navActive[0].innerText}`;
-
-        //rewrite variable
-        navActive = document.getElementsByClassName("navActive");
-
-        // if (navActive[0].innerText == "Starters") {
-        //     console.log(navActive[0]);
-        // }
-    })
-}
-
 //choice in menu
 let choiceMenu = document.createElement("div");
 choiceMenu.className="container choiceMenu";
@@ -92,7 +70,6 @@ for (let i = 0; i < choiceMenuSelect[0].children.length; i++) {
 
 //dish name text
 let dishNameTags = document.querySelectorAll(".choiceMenu > div > h4");
-console.log(dishNameTags);
 dishNameTags[0].textContent="Magnam Tiste";
 dishNameTags[1].textContent="Aut Luia";
 dishNameTags[2].textContent="Est Eligendi";
@@ -117,3 +94,158 @@ dishPriceTags[2].textContent="$8.95";
 dishPriceTags[3].textContent="$12.95";
 dishPriceTags[4].textContent="$12.95";
 dishPriceTags[5].textContent="$9.95";
+
+for (let i = 0; i < navSelect.children.length; i++) {
+    navSelect.children.item(i).addEventListener("click", (e)=>{
+        
+        //delete old  menu active
+        navActive[0].className="";
+
+        //select menu active
+        e.target.className="navActive";
+
+        //title menu change
+        let titleMenuLSelect = document.querySelector(".titleMenu > h3");
+        titleMenuLSelect.innerText=`${navActive[0].innerText}`;
+
+        //rewrite variable
+        navActive = document.getElementsByClassName("navActive");
+
+        //change info based on menu
+        if (navActive[0].innerText == "Starters") {
+            //dish name text
+            let dishNameTags = document.querySelectorAll(".choiceMenu > div > h4");
+            dishNameTags[0].textContent="Magnam Tiste";
+            dishNameTags[1].textContent="Aut Luia";
+            dishNameTags[2].textContent="Est Eligendi";
+            dishNameTags[3].textContent="Eos Luibusdam";
+            dishNameTags[4].textContent="Eos Luibusdam";
+            dishNameTags[5].textContent="Laboriosam Direva";
+
+            //dish ingrediants text
+            let dishIngrediantsTags = document.querySelectorAll(".choiceMenu > div > p");
+            dishIngrediantsTags[0].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[1].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[2].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[3].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[4].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[5].textContent="Lorem, deren, trataro, filede, berada";
+
+            //dish price text
+            let dishPriceTags = document.querySelectorAll(".choiceMenu > div > h5");
+            dishPriceTags[0].textContent="$5.95";
+            dishPriceTags[1].textContent="$14.95";
+            dishPriceTags[2].textContent="$8.95";
+            dishPriceTags[3].textContent="$12.95";
+            dishPriceTags[4].textContent="$12.95";
+            dishPriceTags[5].textContent="$9.95";
+
+            //img
+            let menuImg = document.querySelectorAll(".choiceMenu > div > img");
+            for (let i = 0; i < menuImg.length; i++) {
+                menuImg.item(i).src=`./public/image/menu/menu-item-${i+1}.png`
+            }
+
+        } else if (navActive[0].innerText == "Breakfast") {
+            //dish name text
+            let dishNameTags = document.querySelectorAll(".choiceMenu > div > h4");
+            dishNameTags[0].textContent="Eos Luibusdam";
+            dishNameTags[1].textContent="Eos Luibusdam";
+            dishNameTags[2].textContent="Est Eligendi";
+            dishNameTags[3].textContent="Aut Luia";
+            dishNameTags[4].textContent="Laboriosam Direva";
+            dishNameTags[5].textContent="Magnam Tiste";
+
+            //dish ingrediants text
+            let dishIngrediantsTags = document.querySelectorAll(".choiceMenu > div > p");
+            dishIngrediantsTags[0].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[1].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[2].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[3].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[4].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[5].textContent="Lorem, deren, trataro, filede, berada";
+
+            //dish price text
+            let dishPriceTags = document.querySelectorAll(".choiceMenu > div > h5");
+            dishPriceTags[0].textContent="$7.95";
+            dishPriceTags[1].textContent="$9.95";
+            dishPriceTags[2].textContent="$8.95";
+            dishPriceTags[3].textContent="$8.95";
+            dishPriceTags[4].textContent="$14.95";
+            dishPriceTags[5].textContent="$12.95";
+
+            //img
+            let menuImg = document.querySelectorAll(".choiceMenu > div > img");
+            for (let i = 0; i < menuImg.length; i++) {
+                menuImg.item(i).src=`./public/image/menuBreakfast/menu-item-${i+1}.png`
+            }
+
+        } else if (navActive[0].innerText == "Lunch") {
+            //dish name text
+            let dishNameTags = document.querySelectorAll(".choiceMenu > div > h4");
+            dishNameTags[0].textContent="Magnam Tiste";
+            dishNameTags[1].textContent="Est Eligendi";
+            dishNameTags[2].textContent="Magnam Tiste";
+            dishNameTags[3].textContent="Aut Luia";
+            dishNameTags[4].textContent="Laboriosam Direva";
+            dishNameTags[5].textContent="Eos Luibusdam";
+
+            //dish ingrediants text
+            let dishIngrediantsTags = document.querySelectorAll(".choiceMenu > div > p");
+            dishIngrediantsTags[0].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[1].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[2].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[3].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[4].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[5].textContent="Lorem, deren, trataro, filede, berada";
+
+            //dish price text
+            let dishPriceTags = document.querySelectorAll(".choiceMenu > div > h5");
+            dishPriceTags[0].textContent="$12.95";
+            dishPriceTags[1].textContent="$8.95";
+            dishPriceTags[2].textContent="$9.95";
+            dishPriceTags[3].textContent="$14.95";
+            dishPriceTags[4].textContent="$8.95";
+            dishPriceTags[5].textContent="$8.95";
+
+            //img
+            let menuImg = document.querySelectorAll(".choiceMenu > div > img");
+            for (let i = 0; i < menuImg.length; i++) {
+                menuImg.item(i).src=`./public/image/menuLunch/menu-item-${i+1}.png`
+            }
+        } else if (navActive[0].innerText == "Dinner") {
+            //dish name text
+            let dishNameTags = document.querySelectorAll(".choiceMenu > div > h4");
+            dishNameTags[0].textContent="TisteEos Luibusdam";
+            dishNameTags[1].textContent="Luibusdam";
+            dishNameTags[2].textContent="Magnam Magnam ";
+            dishNameTags[3].textContent="Aut Luia";
+            dishNameTags[4].textContent="Direva";
+            dishNameTags[5].textContent="Est EligendiEos ";
+
+            //dish ingrediants text
+            let dishIngrediantsTags = document.querySelectorAll(".choiceMenu > div > p");
+            dishIngrediantsTags[0].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[1].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[2].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[3].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[4].textContent="Lorem, deren, trataro, filede, berada";
+            dishIngrediantsTags[5].textContent="Lorem, deren, trataro, filede, berada";
+
+            //dish price text
+            let dishPriceTags = document.querySelectorAll(".choiceMenu > div > h5");
+            dishPriceTags[0].textContent="$9.95";
+            dishPriceTags[1].textContent="$9.95";
+            dishPriceTags[2].textContent="$9.95";
+            dishPriceTags[3].textContent="$10.95";
+            dishPriceTags[4].textContent="$11.95";
+            dishPriceTags[5].textContent="$8.95";
+
+            //img
+            let menuImg = document.querySelectorAll(".choiceMenu > div > img");
+            for (let i = 0; i < menuImg.length; i++) {
+                menuImg.item(i).src=`./public/image/menuDinner/menu-item-${i+1}.png`
+            }
+        }
+    })
+}
