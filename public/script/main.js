34bef1cedaaa59424a22d2fa4727659d2195e0b2
vicfir <vicfir@hotmail.fr>
2023-02-02 11:39:24 +0100

    dark mode bg

--- public/script/main.js
+++ public/script/main.js
@@ -320,17 +320,27 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 } 
+
+//-----------------------------------------------------------
+//Dark/light mode
+let btnMode = document.querySelector(".changeMode");
+
+btnMode.addEventListener("click",()=>{
+    let body = document.body;
+
+    body.classList.toggle("darkMode");
+})
